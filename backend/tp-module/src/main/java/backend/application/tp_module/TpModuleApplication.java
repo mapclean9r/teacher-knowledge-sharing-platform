8709de17364c0668bcb3b86a3c2589e58d34c3cc
mapclean9r <tobias.the@hotmail.no>
2026-03-20 20:40:54 +0100

    spring added

---
+++ backend/tp-module/src/main/java/backend/application/tp_module/TpModuleApplication.java
@@ -0,0 +1,13 @@
+package backend.application.tp_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpModuleApplication.class, args);
+	}
+
+}
